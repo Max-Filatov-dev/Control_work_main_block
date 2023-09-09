a0cadad879e8768886136f1e06925025287cea3e
Max_Filatov <maxsthm@mail.ru>
2023-09-09 11:07:26 +0300

    implementation of the main method

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\nЗадача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.\n");
+
+        string[] example = { "Hello", "TV", "World", "Max" };
+
+        int GetLenNewArray(string[] arr)
+        {
+            int len = 0;
+            foreach (var item in arr)
+                if (item.Length <= 3) len++;
+
+            return len;
+        }
+
+        string[] NewArray(string[] arr, int len)
+        {
+            string[] newArray = new string[len];
+            int index = 0;
+            foreach (var item in arr)
+                if (item.Length <= 3)
+                {
+                    newArray[index] = item;
+                    index++;
+                }
+
+            return newArray;
+        }
+
+        void PrintArray(string[] arr, string text)
+        {
+            Console.WriteLine($"{text}");
+            foreach (var item in arr)
+                Console.Write($"{item}, ");
+            Console.WriteLine();
+        }
+
+        int respLen = GetLenNewArray(arr: example);
+        string[] respNew = NewArray(arr: example, len: respLen);
+        PrintArray(arr: example, text: "Исходный массив:");
+        Console.WriteLine();
+        PrintArray(arr: respNew, text: "Новый массив:");
+    }
+}
